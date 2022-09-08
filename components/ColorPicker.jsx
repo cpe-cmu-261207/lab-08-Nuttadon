2d538564474182a909151ca22a1e229ed91366fc
Nuttadon <nuttadon_p@cmu.ac.th>
2022-09-08 14:51:16 +0700

    Gogo

--- components/ColorPicker.jsx
+++ components/ColorPicker.jsx
@@ -1,30 +1,23 @@
 import React, { useContext } from "react";
 import { PainterContext } from "../contexts/PainterContext";
+import { useState } from "react";
 
 export default function ColorPicker(props) {
   const color = props.color;
 
   const { selColor, setSelColor } = useContext(PainterContext);
-
   return (
     <div
+      onClick={() => {
+        setSelColor(color);
+      }}
       style={{
         backgroundColor: color,
         width: "50px",
         height: "50px",
         borderStyle: "solid",
-
-        //if this color is selected, show this
-        //borderColor: "magenta",
-        //borderWidth: "7px",
-
-        //if this color is not selected, show this
-        //borderColor: "black",
-        //borderWidth: "2px",
-      }}
-      onClick={() => {
-        //set selecting color when clicked
-        //your code here
+        borderColor: selColor === color ? "magenta" : "black",
+        borderWidth: selColor === color ? "7px" : "2px",
       }}
     />
   );
